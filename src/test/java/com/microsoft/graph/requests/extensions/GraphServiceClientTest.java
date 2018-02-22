2285a56e0fc4e8d3c610d927d8ac29b0e268d574
Dave Moten <davidmoten@gmail.com>
2018-02-22 14:33:47 +1100

    GraphServiceClient builder rework

--- src/test/java/com/microsoft/graph/requests/extensions/GraphServiceClientTest.java
+++ src/test/java/com/microsoft/graph/requests/extensions/GraphServiceClientTest.java
@@ -2,22 +2,17 @@ package com.microsoft.graph.requests.extensions;
 
 import static org.junit.Assert.assertEquals;
 
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import com.microsoft.graph.authentication.IAuthenticationProvider;
-import com.microsoft.graph.http.HttpMethod;
 import com.microsoft.graph.http.IHttpRequest;
 import com.microsoft.graph.logger.ILogger;
 import com.microsoft.graph.logger.LoggerLevel;
 import com.microsoft.graph.models.extensions.IGraphServiceClient;
-import com.microsoft.graph.options.HeaderOption;
-import com.microsoft.graph.options.Option;
 
 public class GraphServiceClientTest {
 
@@ -26,37 +21,41 @@ public class GraphServiceClientTest {
         final List<String> logs = new CopyOnWriteArrayList<String>();
         IGraphServiceClient client = GraphServiceClient //
                 .builder() //
-                .authenticationProvider(createAuthProviderProvider())
-                .logger(createLoggerProvider(logs)) //
+                .authenticationProvider(createAuthProviderProvider()).logger(createLoggerProvider(logs)) //
                 .buildClient();
         client.getLogger().logDebug("hello");
-        client.getAuthenticationProvider().authenticateRequest(createRequest());
-        assertEquals(Arrays.asList("creating authProvider", "hello", "authenticateRequest called"), logs);
+        client.getAuthenticationProvider().authenticateRequest(null);
+        assertEquals(Arrays.asList("created logger", "created authProvider", "hello", "authenticateRequest called"),
+                logs);
     }
 
     private static IClientConfigProvider<IAuthenticationProvider> createAuthProviderProvider() {
         return new IClientConfigProvider<IAuthenticationProvider>() {
-                ILogger log;
-                IAuthenticationProvider auth;
+            ILogger log;
+            IAuthenticationProvider auth;
+
             @Override
             public IAuthenticationProvider get(IClientConfigFactory factory) {
                 if (log == null) {
                     log = factory.getLogger(factory);
-                    log.logDebug("creating authProvider");
                     auth = new IAuthenticationProvider() {
                         @Override
                         public void authenticateRequest(IHttpRequest request) {
                             log.logDebug("authenticateRequest called");
-                        }};
+                        }
+                    };
+                    log.logDebug("created authProvider");
                 }
                 return auth;
-                
-            }};
+
+            }
+        };
     }
 
     private static IClientConfigProvider<ILogger> createLoggerProvider(final List<String> logs) {
         return new IClientConfigProvider<ILogger>() {
             ILogger log;
+
             @Override
             public ILogger get(IClientConfigFactory factory) {
                 if (log == null) {
@@ -64,7 +63,7 @@ public class GraphServiceClientTest {
 
                         @Override
                         public void setLoggingLevel(LoggerLevel level) {
-                            
+
                         }
 
                         @Override
@@ -74,54 +73,19 @@ public class GraphServiceClientTest {
 
                         @Override
                         public void logDebug(String message) {
-                           logs.add(message);
+                            logs.add(message);
                         }
 
                         @Override
                         public void logError(String message, Throwable throwable) {
                             throwable.printStackTrace();
-                        }};
+                        }
+                    };
+                    log.logDebug("created logger");
                 }
                 return log;
             }
         };
-    }
-
-    private static IHttpRequest createRequest() {
-        return new IHttpRequest() {
-
-            @Override
-            public URL getRequestUrl() {
-                return null;
-            }
-
-            @Override
-            public HttpMethod getHttpMethod() {
-                return null;
-            }
-
-            @Override
-            public List<HeaderOption> getHeaders() {
-                return null;
-            }
-
-            @Override
-            public List<Option> getOptions() {
-                return null;
-            }
-
-            @Override
-            public void addHeader(String header, String value) {
-            }
-
-            @Override
-            public void setUseCaches(boolean useCaches) {
-            }
-
-            @Override
-            public boolean getUseCaches() {
-                return false;
-            }};
     }
 
 }
